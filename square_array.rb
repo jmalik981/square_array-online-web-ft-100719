b8c48dea2a08210da4c84c030492a776fbb090aa
Junaid Malik <jmalik981@gmail.com>
2019-10-21 14:01:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-  # new_array = []
-  # array.each do |number|
-  # new_array<< number**2
-  # end 
-  # new_array
-  array.map {|number| number**2}
+  new_array = []
+  array.each do |number|
+  new_array<< number**2
+  end 
+  new_array
+#  array.map {|number| number**2}
 end
 
